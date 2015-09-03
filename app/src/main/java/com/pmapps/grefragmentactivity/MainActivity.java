17f1fffa394dadcf43dc549ca3d6dd08a261d8ef
Dushyant Sharma <sendtods@gmail.com>
2015-09-03 23:51:30 +0530

    Now, On the way of building the full GrePictionary Lite App. Added Four two more buttons to fragmentactivity.BtnPicture and BtnMeaning

---
+++ app/src/main/java/com/pmapps/grefragmentactivity/MainActivity.java
@@ -0,0 +1,63 @@
+package com.pmapps.grefragmentactivity;
+
+import android.support.v4.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends FragmentActivity implements FragmentOne.OnTextChangedListener{
+    Button btnNext,btnPrevious;
+    String TVnumber;
+    Integer tvnum;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnNext = (Button)findViewById(R.id.btnNext);
+        btnPrevious = (Button)findViewById(R.id.btnPrevious);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              TVnumber = GetTextFromTVDisplay();
+              tvnum = Integer.parseInt(TVnumber);
+                if(tvnum==10)
+                {
+                    Toast.makeText(MainActivity.this,"Last Number!Can't Go Beyond That",Toast.LENGTH_LONG).show();
+                }else
+                {   tvnum = tvnum + 1;
+                    TVnumber = tvnum.toString();
+                    FragmentOne f1 = (FragmentOne)getSupportFragmentManager().findFragmentById(R.id.fragment_id);
+                    f1.updateTVDisplay(TVnumber);
+
+                }
+            }
+        });
+        btnPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TVnumber = GetTextFromTVDisplay();
+                tvnum = Integer.parseInt(TVnumber);
+                if(tvnum==1)
+                {
+                    Toast.makeText(MainActivity.this,"First Number!Can't Go Beyond That",Toast.LENGTH_LONG).show();
+                }else
+                {   tvnum = tvnum - 1;
+                    TVnumber = tvnum.toString();
+                    FragmentOne f1 = (FragmentOne)getSupportFragmentManager().findFragmentById(R.id.fragment_id);
+                    f1.updateTVDisplay(TVnumber);
+
+                }
+            }
+        });
+    }
+
+
+
+    @Override
+    public String GetTextFromTVDisplay() {
+        FragmentOne f1 = (FragmentOne)getSupportFragmentManager().findFragmentById(R.id.fragment_id);
+        return f1.GetTextFromDisplay();
+    }
+}
